1905ce432ae7abcc55d80ca17b7a3f6437c8d71a
abik11 <abik11@gmail.com>
2019-01-04 08:35:31 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 ﻿require.context("../img/", true, /\.(jpe?g|png|gif|svg)$/i);
 require('es6-promise').polyfill();
+import Polyfill from './core/es6-polyfill.js';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueCordova from './device/vue-cordova.js';
@@ -19,7 +20,8 @@ import ShowMac from './components/showMac.vue';
    function onDeviceReady() {
       document.addEventListener('pause', onPause.bind(this), false);
       document.addEventListener('resume', onResume.bind(this), false);
-
+      
+      Polyfill.objectAssign();
       Vue.use(VueRouter);
       Vue.use(VueCordova);
       Vue.use(VueI18n);
