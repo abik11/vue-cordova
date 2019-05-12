c74f2dea813391021b7a28544b9e1b7b3872d41c
abik11 <abik11@gmail.com>
2019-05-12 16:15:12 +0200

    Create circularLoader.vue

---
+++ src/js/components/circularLoader.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="progress-loader">
+      <v-progress-circular :size="size" :width="width" color="primary" indeterminate>
+      </v-progress-circular>
+   </div>
+</template>
+ 
+<script>
+   export default {
+      name: 'cornerLoader',
+      props: {
+         size: {
+            type: Number,
+            default: 20
+         },
+         width: {
+            type: Number,
+            default: 4
+         }
+      }
+   }
+</script>
+ 
+<style lang="scss" scoped>
+   .progress-loader {
+      position: absolute;
+      top: 5px;
+      right: 5px;
+      z-index: 1000;
+   }
+</style>
